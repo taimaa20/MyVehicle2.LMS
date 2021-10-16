29b2343c587aa5f5a51efeaba1b3192cd90b8133
Qussai AL_Doghaem <qussai.doghaem@gmail.com>
2021-10-16 22:49:19 +0300

    AddUserRepository

---
+++ MyVehicle.LMS.CORE/Repoisitory/IUsersRepoisitory.cs
@@ -0,0 +1,18 @@
+﻿using MyVehicle.LMS.CORE.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyVehicle.LMS.CORE.Repoisitory
+{
+  public  interface IUsersRepoisitory
+    {
+        public bool InsertUsers(Users users);
+        public bool UpdateUsers(Users users);
+        public bool DeleteUsers(Users users);
+        public List<Users> GetAllUsers();
+        public Users GetAllUsersById(Users users);
+        
+
+    }
+}
---
+++ MyVehicle.LMS.INFRA/Repoisitory/UsersRepoisitory.cs
@@ -0,0 +1,89 @@
+﻿using Dapper;
+using MyVehicle.LMS.CORE.Common;
+using MyVehicle.LMS.CORE.Data;
+using MyVehicle.LMS.CORE.Repoisitory;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace MyVehicle.LMS.INFRA.Repoisitory
+{
+    public class UsersRepoisitory : IUsersRepoisitory
+    {
+        private readonly IDBContext dBContext;
+        public UsersRepoisitory(IDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+        public bool DeleteUsers(Users users)
+        {
+            var Parameters = new DynamicParameters();
+
+            Parameters.Add("@UserId", users.UserId, dbType: DbType.Int32, direction: ParameterDirection.Input);
+           
+            var Reselt = dBContext.Connection.ExecuteAsync("UpdateUsers", Parameters, commandType: CommandType.StoredProcedure);
+            return true;
+        }
+
+        public List<Users> GetAllUsers()
+        {
+            IEnumerable<Users> reselt = dBContext.Connection.Query<Users>("GetAllUsers", commandType: CommandType.StoredProcedure);
+
+            return reselt.ToList();
+        }
+
+        public Users GetAllUsersById(Users users)
+        {
+            var Parameter = new DynamicParameters();
+            Parameter.Add("@UserId", users.UserId, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            var reselt = dBContext.Connection.Query<Users>("GetAllUsersById", Parameter, commandType: CommandType.StoredProcedure);
+
+            return reselt.FirstOrDefault();
+        }
+
+        public bool InsertUsers(Users users)
+        {
+            var Parameters = new DynamicParameters();
+            Parameters.Add("@FullName", users.FullName, dbType: DbType.String, direction: ParameterDirection.Input);
+            Parameters.Add("@UserImage", users.UserImage, dbType: DbType.String, direction: ParameterDirection.Input);
+            Parameters.Add("@Gender", users.Gender, dbType: DbType.Boolean, direction: ParameterDirection.Input);
+            Parameters.Add("@Age", users.Age, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            Parameters.Add("@Email", users.Email, dbType: DbType.String, direction: ParameterDirection.Input);
+            Parameters.Add("@PhoneNumber", users.PhoneNumber, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            Parameters.Add("@Salary", users.Salary, dbType: DbType.Double, direction: ParameterDirection.Input);
+            Parameters.Add("@Address", users.Address, dbType: DbType.String, direction: ParameterDirection.Input);
+            Parameters.Add("@Username", users.Username, dbType: DbType.String, direction: ParameterDirection.Input);
+            Parameters.Add("@Password", users.Password, dbType: DbType.String, direction: ParameterDirection.Input);
+            Parameters.Add("@RoleId", users.RoleId, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            Parameters.Add("@SettingId", users.SettingId, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            var Reselt = dBContext.Connection.ExecuteAsync("InsertUsers", Parameters, commandType: CommandType.StoredProcedure);
+            return true;
+
+        }
+
+        public bool UpdateUsers(Users users)
+        {
+            var Parameters = new DynamicParameters();
+            
+            Parameters.Add("@UserId", users.UserId, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            Parameters.Add("@FullName", users.FullName, dbType: DbType.String, direction: ParameterDirection.Input);
+            Parameters.Add("@UserImage", users.UserImage, dbType: DbType.String, direction: ParameterDirection.Input);
+            Parameters.Add("@Gender", users.Gender, dbType: DbType.Boolean, direction: ParameterDirection.Input);
+            Parameters.Add("@Age", users.Age, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            Parameters.Add("@Email", users.Email, dbType: DbType.String, direction: ParameterDirection.Input);
+            Parameters.Add("@PhoneNumber", users.PhoneNumber, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            Parameters.Add("@Salary", users.Salary, dbType: DbType.Double, direction: ParameterDirection.Input);
+            Parameters.Add("@Address", users.Address, dbType: DbType.String, direction: ParameterDirection.Input);
+            Parameters.Add("@Username", users.Username, dbType: DbType.String, direction: ParameterDirection.Input);
+            Parameters.Add("@Password", users.Password, dbType: DbType.String, direction: ParameterDirection.Input);
+            Parameters.Add("@RoleId", users.RoleId, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            Parameters.Add("@SettingId", users.SettingId, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            var Reselt = dBContext.Connection.ExecuteAsync("UpdateUsers", Parameters, commandType: CommandType.StoredProcedure);
+            return true;
+        }
+
+      
+    }
+}
