811cf598c2e2398290e01e8a9f64516051105eea
taimaahamarneh <taimaahamarneh1@gmail.com>
2021-10-17 11:54:35 +0300

    add-notification repositoryand service

---
+++ MyVehicle.LMS.CORE/Repoisitory/INotificationRepository.cs
@@ -0,0 +1,15 @@
+﻿using MyVehicle.LMS.CORE.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyVehicle.LMS.CORE.Repoisitory
+{
+    public interface INotificationRepository
+    {
+        public bool DeleteNotification(Notification notification);
+        public bool InsertNotification(Notification notification);
+        public bool UpdateNotification(Notification notification);
+        public List<Notification> GetAllNotification();
+    }
+}
