f31c8318c9ef98784f1342ad7fd798ba42f68f63
THORAYYA BANY YASIN <tsbanyyasin15@cit.just.edu.jo>
2021-10-23 14:59:05 +0300

    Add JWT

---
+++ MyVehicle.LMS.CORE/Services/IJwtService.cs
@@ -0,0 +1,12 @@
+﻿using MyVehicle.LMS.CORE.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyVehicle.LMS.CORE.Services
+{
+   public interface IJwtService
+    {
+        public string Auth(UsernamePassword usernamePassword);
+    }
+}
