427594f34d984311c1d9334646bb546320815c87
THORAYYA BANY YASIN <tsbanyyasin15@cit.just.edu.jo>
2021-11-16 20:54:08 +0200

    updatejwt

---
+++ MyVehicle.LMS.CORE/DTO/UserId.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyVehicle.LMS.CORE.DTO
+{
+   public static class UserId
+    {
+        public const String Id = "UserId";
+    }
+}
